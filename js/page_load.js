23f100173c49cd159a729a197e919cf9cff929c3
drshyam1974 <115407330+drshyam1974@users.noreply.github.com>
2026-08-08 22:44:01 +0530

    Add files via upload

---
+++ js/page_load.js
@@ -0,0 +1,19 @@
+// Author: Mr. Beeren Sahu
+// Date: 05-Aug-2012
+
+
+function loadContent_top()
+{
+   $("#include-top").load("my_include/top.html");
+}
+
+function loadContent_right()
+{
+   $("#include-right").load("my_include/right.html");
+}
+
+function loadContent_bottom()
+{
+   $("#include-bottom").load("my_include/bottom.html");
+}
+
